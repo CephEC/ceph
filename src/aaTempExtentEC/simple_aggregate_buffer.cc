3dbf323339736ace047425d7cc11be3a7fae037b
amoeyo <931402924@qq.com>
2022-11-25 18:46:15 +0800

    src/aaTempExtentEC/: 完善写逻辑然后开摆
    Signed-off-by: Shuai Xiaoyu <931402924@qq.com>

--- src/aaTempExtentEC/simple_aggregate_buffer.cc
+++ src/aaTempExtentEC/simple_aggregate_buffer.cc
@@ -14,21 +14,42 @@ int SimpleAggregateBuffer::write(MOSDOp* op, const OSDMap &osdmap)
             SimpleVolume* last_vol = volumes.back();
         }
         else {
-            // 逆序遍历volume list
+            // TODO：查volume元数据
+            // 先逆序遍历volume list看是否有未往下写的未满volume
             std::list<SimpleVolume*>::iterator i = volumes.rbegin();
             while(i != volumes.rend()) {
-                if (!(*i)->full()) {
+                if (!(*i)->full() && !(*i)->flushing()) {
                     last_vol = *i;
                     break;
                 }
                 i++;
             }
+            
+            // buffer中没有空闲vol，查找volume元数据缓存
+            if (i == volumes.rend()) {
+                std::vector<volume_t>::iterator vol = volume_meta_cache.begin();
+                while (vol != volume_meta_cache.end()) {
+                    if (!vol.full()) {
+                        // TODO: 根据元数据读取数据块和EC块到osd，构建volume
+                        // 是在这一步读取还是延迟读取？元数据一致性如何保证？
+
+                        last_vol = new SimpleVolume(SimpleVolume::default_volume_capacity, this);
+                        // add_chunk 
+
+                    }
+                }
+
+            }
         }
     
-    } else {
-        last_vol = new SimpleVolume(SimpleVolume::default_volume_capacity, this);
     }
 
+    // 卷已满（针对批量写入情况的末尾卷）或者为空
+    if(!last_vol || last_vol->full()) {
+        last_vol = new SimpleVolume(SimpleVolume::default_volume_capacity, this);
+    }
+    
+    // 向卷中添加chunk
     if (last_vol) {
         r = last_vol->add_chunk(op, osdmap);
         if (r < 0) {
