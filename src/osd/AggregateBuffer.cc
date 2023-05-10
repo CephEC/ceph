b597ae907bf6eabe4bd7314885c8ffabeb6c08c6
wwow1 <2534479048@qq.com>
2023-05-10 10:20:09 +0800

    AggregateBuffer::op_translate 在转译读请求过程中，需要保留client实际想要读取的length，而不是默认client全量读取rados对象
    Signed-off-by: Zheng Fuyu <2534479048@qq.com>

--- src/osd/AggregateBuffer.cc
+++ src/osd/AggregateBuffer.cc
@@ -344,7 +344,8 @@ int AggregateBuffer::op_translate(MOSDOp* m) {
 
       // READ, CALL, WRITEFULL公用的转译逻辑
       uint64_t vol_offset = uint8_t(chunk_meta.get_chunk_id()) * chunk_meta.get_chunk_size();
-      uint64_t vol_length = chunk_meta.get_offset();
+      uint64_t vol_length = osd_op.op.extent.length > chunk_meta.get_offset() ?
+        chunk_meta.get_offset() : osd_op.op.extent.length;
       // 将待访问RGW对象的oid,off,len替换为volume对象的oid,off,len
       if (osd_op.op.extent.offset == chunk_meta.get_chunk_size()) {
         // offset超出RGW对象本身的大小，本次不读取任何数据
